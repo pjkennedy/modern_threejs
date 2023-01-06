d70f733372e6a2fdfec8da500608daec0557eaed
Patrick Kennedy <kennedypj@gmail.com>
2023-01-06 17:06:05 -0600

    Up thru 6; short lesson

--- main.js
+++ main.js
@@ -18,37 +18,43 @@ renderer.setPixelRatio(devicePixelRatio)
 document.body.appendChild(renderer.domElement)
 
 
-/// removing the yellow box ///
-const boxGeometry = new THREE.BoxGeometry(1, 1, 1)
-const material = new THREE.MeshBasicMaterial({ color: 0x00ff00})
+/// removing the yellow box; next two ///
+///const boxGeometry = new THREE.BoxGeometry(1, 1, 1)
+///const material = new THREE.MeshBasicMaterial({ color: 0x00ff00})
 //console.log(boxGeometry)
 //console.log(material)
 
-const mesh = new THREE.Mesh( boxGeometry, material)
+///const mesh = new THREE.Mesh( boxGeometry, material)
 //console.log(mesh)
-scene.add(mesh)
+/// removing the yellow box code ///
+//scene.add(mesh)
 /// end removing the yellow box code ///
 
 
 camera.position.z = 5
 
 const planeGeometry = new THREE.PlaneGeometry(5, 5, 10, 10)
-const planeMaterial = new THREE.MeshBasicMaterial( 
+const planeMaterial = new THREE.MeshPhongMaterial( 
   {color: 0xff0000, side: THREE.DoubleSide} )
 const planeMesh = new THREE.Mesh(
   planeGeometry, planeMaterial
 )
 scene.add(planeMesh) 
 
-
+const light = new THREE.DirectionalLight(
+  0xffffff, 1
+)
+light.position.set(0, 0, 1)
+scene.add(light)
 
 function animate() {
   requestAnimationFrame(animate)
   renderer.render(scene, camera)
   /// next two are for the yellow box; comment out if not needed ///
-  mesh.rotation.x += 0.01
-  mesh.rotation.y += 0.01
-  planeMesh.rotation.x += 0.01    // if you don't want the plane to rotate
+  ///mesh.rotation.x += 0.01
+  ///mesh.rotation.y += 0.01
+  /// and stop rotation the red plane too
+  ///planeMesh.rotation.x += 0.01    // if you don't want the plane to rotate
   
 } 
 
