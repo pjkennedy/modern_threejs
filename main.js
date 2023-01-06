7dc5af2217583e82a2442b730f38c22cea54a7a8
Patrick Kennedy <kennedypj@gmail.com>
2023-01-06 17:48:40 -0600

    Created a random surface on plane. #7

--- main.js
+++ main.js
@@ -34,12 +34,28 @@ document.body.appendChild(renderer.domElement)
 camera.position.z = 5
 
 const planeGeometry = new THREE.PlaneGeometry(5, 5, 10, 10)
-const planeMaterial = new THREE.MeshPhongMaterial( 
-  {color: 0xff0000, side: THREE.DoubleSide} )
+const planeMaterial = new THREE.MeshPhongMaterial({
+  color: 0xff0000, 
+  side: THREE.DoubleSide, 
+  flatShading: THREE.FlatShading
+})
+
 const planeMesh = new THREE.Mesh(
   planeGeometry, planeMaterial
 )
 scene.add(planeMesh) 
+console.log(planeMesh)
+
+const {array} = planeMesh.geometry
+.attributes.position
+for( let i = 3; i < array.length; i+=3)
+  {
+    const x = array[i]
+    const y = array[i+1]
+    const z = array[i+2]
+    
+    array[i + 2] = Math.random()
+  }
 
 const light = new THREE.DirectionalLight(
   0xffffff, 1
