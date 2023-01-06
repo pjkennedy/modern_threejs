bf7ed368a0f7da1f224396919beb81dcbf01ec3c
Patrick Kennedy <kennedypj@gmail.com>
2023-01-05 18:32:22 -0600

    Finish up thru part 5

--- main.js
+++ main.js
@@ -17,24 +17,36 @@ renderer.setSize(innerWidth, innerHeight)
 renderer.setPixelRatio(devicePixelRatio)
 document.body.appendChild(renderer.domElement)
 
+/*
+/// removing the yellow box
 const boxGeometry = new THREE.BoxGeometry(1, 1, 1)
 const material = new THREE.MeshBasicMaterial({ color: 0x00ff00})
 //console.log(boxGeometry)
 //console.log(material)
 
-
 const mesh = new THREE.Mesh( boxGeometry, material)
-console.log(mesh)
-
+//console.log(mesh)
 scene.add(mesh)
-
+*/
 camera.position.z = 5
+
+const planeGeometry = new THREE.PlaneGeometry(5, 5, 10, 10)
+const planeMaterial = new THREE.MeshBasicMaterial( 
+  {color: 0xff0000, side: THREE.DoubleSide} )
+const planeMesh = new THREE.Mesh(
+  planeGeometry, planeMaterial
+)
+scene.add(planeMesh) 
+
+
 
 function animate() {
   requestAnimationFrame(animate)
   renderer.render(scene, camera)
-  mesh.rotation.x += 0.01
-  mesh.rotation.y += 0.01
+  ///mesh.rotation.x += 0.01
+  ///mesh.rotation.y += 0.01
+  ///planeMesh.rotation.x += 0.01    // if you don't want the plane to rotate
+  
 } 
 
 animate()
