a30b63273f6b53dc059f4dd15878c98aa0c0972f
Patrick Kennedy <kennedypj@gmail.com>
2023-01-09 22:37:19 -0600

    Up thru #8; can modify plan with dat.gui

--- main.js
+++ main.js
@@ -2,7 +2,49 @@
 
 // Import the entire three.js core library, if needed
 import * as THREE from 'three';
+import * as dat from 'dat.gui'
 
+const gui = new dat.GUI()
+console.log(gui)
+const world = {
+  plane: {
+    width: 10,
+    height: 10,
+    widthSegments: 10,
+    heightSegments: 10
+  }  
+}
+gui.add(world.plane, 'width', 1, 20).
+  onChange(generatePlane)  
+
+  gui.add(world.plane, 'height', 1, 20).
+  onChange(generatePlane)
+
+  gui.add(world.plane, 'widthSegments', 1, 50).
+  onChange(generatePlane)
+
+  gui.add(world.plane, 'heightSegments', 1, 50).
+  onChange(generatePlane)
+
+function generatePlane() {
+  planeMesh.geometry.dispose()
+  planeMesh.geometry = new THREE.PlaneGeometry
+    (world.plane.width, 
+     world.plane.height, 
+     world.plane.widthSegments, 
+     world.plane.heightSegments)
+
+    const {array} = planeMesh.geometry
+    .attributes.position
+    for( let i = 3; i < array.length; i+=3)
+      {
+        const x = array[i]
+        const y = array[i+1]
+        const z = array[i+2]
+        
+        array[i + 2] = Math.random()
+      }
+}
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000)
@@ -44,7 +86,7 @@ const planeMesh = new THREE.Mesh(
   planeGeometry, planeMaterial
 )
 scene.add(planeMesh) 
-console.log(planeMesh)
+
 
 const {array} = planeMesh.geometry
 .attributes.position
